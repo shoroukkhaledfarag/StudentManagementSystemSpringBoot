fffb866fe8b3320d675c803a72143a99cc342940
shorouk.khaled <shorouk.khaled@gizasystems.com>
2022-06-11 08:24:29 +0200

    Initial commit

---
+++ src/main/java/com/example/StudentManagementSystemSpringBoot/StudentManagementSystemSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentManagementSystemSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StudentManagementSystemSpringBoot/StudentManagementSystemSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentManagementSystemSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
